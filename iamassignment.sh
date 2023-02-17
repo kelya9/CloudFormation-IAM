fbea6d5497646c613b45b1467424684cecec3271
kelya9 <94655040+kelya9@users.noreply.github.com>
2023-02-16 23:30:20 -0500

    replaced hardcodes values with variables

--- iamassignment.sh
+++ iamassignment.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
-
+#Author:Nicole Takam
+#Creation Date:09-01-2023
 #create policy awsaccess for only ....
 
+read -p "Enter a groupname : " groupname
+echo " The groupname is $groupname"
+sleep 2
+
+read -p "Enter a username : " username
+
+echo " The username is $username"
+sleep 2
+
+read -p "Enter a password for user : " password
+sleep 2
 
 aws iam create-policy --policy-name awsaccess --policy-document file://accesspolicy.json | grep -i arn
 
 # Create a group called devopadmins
 
-aws iam create-group --group-name devopadmins
+aws iam create-group --group-name $groupname
 
 # Attach policy to the group
 
-aws iam attach-group-policy --policy-arn arn:aws:iam::145682747025:policy/awsaccess --group-name devopadmins
+aws iam attach-group-policy --policy-arn arn:aws:iam::145682747025:policy/awsaccess --group-name $groupname
 
 # Create a user with username and password
 
-aws iam create-user --user-name sephora
+aws iam create-user --user-name $username
 
-aws iam create-login-profile --user-name sephora --password 'Blue3567!' --password-reset-required
+aws iam create-login-profile --user-name $username --password '$password' --password-reset-required
 
 #Add user to the group
 
-aws iam add-user-to-group --user-name sephora --group-name devopadmins
+aws iam add-user-to-group --user-name $username --group-name $groupname
